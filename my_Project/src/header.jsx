a3abe285a2482fb3165d1361c7dfdb2ec1f8d23e
Mohammad Zaatra <imzaatra@gmail.com>
2025-02-18 13:02:54 +0200

    Remove unused React DOM files and add Vite as a dependency

--- my_Project/src/header.jsx
+++ my_Project/src/header.jsx
@@ -1,16 +1,31 @@
 function Header() {
   return (
-    <header>
-      <h1>I am Ameer Yasen</h1>
-      <nav>
-        <ul>
-          <li>Ameer</li>
-          <li>Ata</li>
-          <li>Ahmad</li>
-          <li>Yasen</li>
-        </ul>
-      </nav>
-    </header>
+      <header className="electricity-maps-header">
+          <div className="header-left">
+              <div className="logo">ELECTRICITY MAPS</div>
+              <nav className="header-nav">
+                  <ul>
+                      <li><a href="/faq">FAQ</a></li>
+                      <li><a href="/methodology">1</a></li>
+                      <li><a href="/careers">2</a></li>
+                      <li><a href="/community">Community</a></li>
+                      <li><a href="/blog">Blog</a></li>
+                  </ul>
+              </nav>
+          </div>
+          <div className="header-right">
+              <div className="toggle-group">
+                  <button className="toggle-button active">Production</button>
+                  <button className="toggle-button">Consumption</button>
+              </div>
+              <div className="toggle-group">
+                  <button className="toggle-button active">Country</button>
+                  <button className="toggle-button">Zone</button>
+                  <span className="info-icon">(i)</span>
+              </div>
+              <button className="zoom-plus">+</button>
+          </div>
+      </header>
   );
 }
 export default Header;
